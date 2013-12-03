ffd350ba454bd2064dcc412a6c76e61c7797157d
andymao <andymao@qq.com>
2013-12-03 15:09:50 +0800

    打算测试tao_sdk的imagebinder

--- src/test/scrolllayout/FooScrollTest.java
+++ src/test/scrolllayout/FooScrollTest.java
@@ -2,17 +2,15 @@ package test.scrolllayout;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager.LayoutParams;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class FooScrollTest extends Activity {
  @Override
 protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	LinearLayout line = new LinearLayout(this);
-	LayoutParams params = new LayoutParams();
-	params.height = LayoutParams.FILL_PARENT;
-	params.width = LayoutParams.FILL_PARENT;
+	LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT);
 	line.setLayoutParams(params);
 	
 	FooScroll scrollView = new FooScroll(this, null);
--- src/test/tao/harness/TestHarness.java
+++ src/test/tao/harness/TestHarness.java
@@ -1,23 +1,35 @@
 package test.tao.harness;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.Contacts.People;
+import android.taobao.common.TaoToolBox;
+import android.taobao.imagebinder.ImageBinder;
+import android.taobao.imagebinder.ImagePoolBinder;
 import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.webkit.CookieManager;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -25,6 +37,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hongyunbasic.R;
+import com.taobao.tao.imagepool.ImageCache;
+import com.taobao.tao.imagepool.ImageGroup;
 
 public class TestHarness extends ListActivity {
 	ListView listView;
@@ -32,78 +46,81 @@ public class TestHarness extends ListActivity {
 	private TextView floatingHead;
 	private Rect mVisibleRect = new Rect();
 	private int mFloatingCategoryHeight;
-	protected String TAG="TestHarness";
-
-	private static String[] GENRES = new String[] {
-        "Action", "Adventure", "Animation", "Children", "Comedy", "Documentary", "Drama",
-        "Foreign", "History", "Independent", "Romance", "Sci-Fi", "Television", "Thriller"
-    };
+	protected String TAG = "TestHarness";
+	private ViewGroup mMainViewGroup;
+	private static String[] GENRES = new String[] { "Action", "Adventure",
+			"Animation", "Children", "Comedy", "Documentary", "Drama",
+			"Foreign", "History", "Independent", "Romance", "Sci-Fi",
+			"Television", "Thriller" };
 	private static final String TEST_PREFIX = "test";
 	private ArrayList<String> mMethodNames = new ArrayList<String>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		testTaoSearchListActivityFloatingTopbar();
-		
-//		String cookie = CookieManager.getInstance().getCookie(url);
 		setContentView(R.layout.testharness);
+		mMainViewGroup = (ViewGroup) findViewById(R.id.main);
+		reflectionCallTestMethods();
+	}
 
-     
-        
-        Class thisClass;
+	private void reflectionCallTestMethods() {
+		// setContentView(R.layout.testharness);
+		Class thisClass;
 		try {
 			thisClass = Class.forName("test.tao.harness.TestHarness");
-		    Method[] methods = thisClass.getMethods();
-	        Method m;
-	        for(int i=0;i<methods.length;i++){
-	        	m = methods[i];
-	        	Log.d(TAG, m.getName());
-	        	if(m.getName().toLowerCase().startsWith(TEST_PREFIX)){
-	        		mMethodNames.add(m.getName());
-	        	}
-	        }
-	        
+			Method[] methods = thisClass.getMethods();
+			Method m;
+			for (int i = 0; i < methods.length; i++) {
+				m = methods[i];
+				Log.d(TAG, m.getName());
+				if (m.getName().toLowerCase().startsWith(TEST_PREFIX)) {
+					mMethodNames.add(m.getName());
+				}
+			}
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-    
-        GENRES =  mMethodNames.toArray(new String[mMethodNames.size()]);
-        System.out.println(GENRES);
-        
-        setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_multiple_choice, GENRES));
-        
-        final ListView listView = getListView();
 
-        listView.setItemsCanFocus(false);
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        
-        Button runBtn = (Button)findViewById(R.id.btn_run);
-        runBtn.setOnClickListener(new OnClickListener() {
-			
+		GENRES = mMethodNames.toArray(new String[mMethodNames.size()]);
+		System.out.println(GENRES);
+
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_multiple_choice, GENRES));
+
+		final ListView listView = getListView();
+
+		listView.setItemsCanFocus(false);
+		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+		Button runBtn = (Button) findViewById(R.id.btn_run);
+		runBtn.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				SparseBooleanArray checked = listView.getCheckedItemPositions();
 				for (int i = 0; i < GENRES.length; i++)
-					 if (checked.get(i)) {
+					if (checked.get(i)) {
 						int pos = i;
-						if(pos>=0 && pos<GENRES.length){
+						if (pos >= 0 && pos < GENRES.length) {
 							runMethod(GENRES[pos]);
-						}else{
-							Toast.makeText(getApplicationContext(), "bad pos number", Toast.LENGTH_SHORT).show();
+						} else {
+							Toast.makeText(getApplicationContext(),
+									"bad pos number", Toast.LENGTH_SHORT)
+									.show();
 						}
-					 }
+					}
 			}
 		});
 	}
-	
+
 	/**
 	 * @param string
 	 */
 	protected void runMethod(String string) {
-		 try {
-			Method method=this.getClass().getDeclaredMethod(string,null);
+		try {
+			Method method = this.getClass().getDeclaredMethod(string, null);
 			try {
 				method.invoke(this, null);
 			} catch (IllegalArgumentException e) {
@@ -122,24 +139,16 @@ public class TestHarness extends ListActivity {
 		}
 	}
 
-	public void test12(){
-		Toast.makeText(getApplicationContext(), "test12", Toast.LENGTH_SHORT).show();
+	public void testCookieManagerOnNullUrl() {
+		String cookie = CookieManager.getInstance().getCookie(null);
+		System.out.println(cookie);
 	}
-	public void test13(){
-		Toast.makeText(getApplicationContext(), "test13", Toast.LENGTH_SHORT).show();
-		
-	}
-	public void test14(){
-		Toast.makeText(getApplicationContext(), "test14", Toast.LENGTH_SHORT).show();
-		
-	}
-	private void test15(){
-		Toast.makeText(getApplicationContext(), "test15", Toast.LENGTH_SHORT).show();
-		
-	}
-	
-	private void testTaoSearchListActivityFloatingTopbar() {
-		setContentView(R.layout.activity_testharness_listview);
+
+	public void testTaoSearchListActivityFloatingTopbar() {
+		mMainViewGroup.removeAllViews();
+		mMainViewGroup.addView(View.inflate(getApplicationContext(),
+				R.layout.activity_testharness_listview, null));
+
 		listView = (ListView) findViewById(R.id.listView1);
 		floatingHead = (TextView) findViewById(R.id.tv_head);
 		header = View.inflate(getApplicationContext(),
@@ -160,11 +169,13 @@ public class TestHarness extends ListActivity {
 				if (floatingHead != null) {
 					header.getLocalVisibleRect(mVisibleRect);
 					ViewParent parent = header.getParent();
-					String log1 = String.format("mVisibleRect.bottom=%s,mVisibleRect.top=%d," +
-							"mFloatingCategoryHeight=%d", 
-							mVisibleRect.bottom,mVisibleRect.top,mFloatingCategoryHeight);
+					String log1 = String.format(
+							"mVisibleRect.bottom=%s,mVisibleRect.top=%d,"
+									+ "mFloatingCategoryHeight=%d",
+							mVisibleRect.bottom, mVisibleRect.top,
+							mFloatingCategoryHeight);
 					Log.d(TAG, log1);
-					
+
 					if (parent == null
 							|| (mVisibleRect.bottom - mVisibleRect.top <= mFloatingCategoryHeight)
 							|| header.getPaddingTop() < 0) {
@@ -193,16 +204,55 @@ public class TestHarness extends ListActivity {
 				// The "text1" view defined in the XML template
 				new int[] { android.R.id.text1 });
 		listView.setAdapter(adapter);
+		listView.invalidate();
 	}
-	
+
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 		// TODO Auto-generated method stub
 		super.onWindowFocusChanged(hasFocus);
-		
-		if(hasFocus && floatingHead!=null){
+
+		if (hasFocus && floatingHead != null) {
 			mFloatingCategoryHeight = floatingHead.getMeasuredHeight();
 		}
+	}
+
+	// END
+	// OF-------------------testTaoSearchListActivityFloatingTopbar--------------
+
+	public void testStrictMode() {
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+				.detectNetwork().penaltyDialog().build();
+
+		StrictMode.setThreadPolicy(policy);
+		InputStream in = null;
+		try {
+			URL url = new URL("ftp://mirror.csclub.uwaterloo.ca/index.html");
+			URLConnection urlConnection = url.openConnection();
+			in = new BufferedInputStream(urlConnection.getInputStream());
+			System.out.println(in.toString());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (in != null)
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+
+	}
+	
+	public void testImageBinder(){
+		mMainViewGroup.removeAllViews();
+		ImageView image = new ImageView(this);
+		mMainViewGroup.addView(image);
+		
+		ImageBinder mImagePoolBinder = new ImagePoolBinder(this.getClass().getName(), getApplication(), ImageGroup.PRIORITY_NORMAL,
+				ImageCache.CACHE_CATEGORY_MRU);
+		String url = "http://gw.alicdn.com/bao/uploaded/i4/17994026573597533/T1QU5oFldcXXXXXXXX_!!0-item_pic.jpg_90x90q90.jpg";
+		mImagePoolBinder.setImageDrawable(url, image);
 	}
 
 }
