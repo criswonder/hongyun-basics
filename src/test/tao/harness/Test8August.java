e294faee1646d303190b950ba5790488d3fa8215
andymao <wb-maohongyun@alibaba-inc.com>
2014-09-22 19:36:43 +0800

    add test image pool files

--- src/test/tao/harness/Test8August.java
+++ src/test/tao/harness/Test8August.java
@@ -1,11 +1,128 @@
 package test.tao.harness;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnAttachStateChangeListener;
+import android.view.View.OnClickListener;
+import android.view.View.OnCreateContextMenuListener;
+import android.view.View.OnDragListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.View.OnGenericMotionListener;
+import android.view.View.OnHoverListener;
+import android.view.View.OnKeyListener;
+import android.view.View.OnLayoutChangeListener;
+import android.view.View.OnLongClickListener;
+import android.view.View.OnSystemUiVisibilityChangeListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+
 //import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class Test8August extends TestBaseActivity{
 	
-
-	public void testMethod1(){
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 	}
+	
+	/**
+	 * 准备获取到显示广告view的clickListener，然后手动去触发
+	 * 结果：美女图片中腾讯的广告并没有设置click listener。
+	 */
+	public void testRelactionGetViewListener(){
+		final Button btn = new Button(this);
+		btn.setText("hitme!");
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				System.out.println("fkkkkkkkkkkk");
+				
+				Class<View> clazz = View.class;
+				
+				try {
+					Field f= clazz.getDeclaredField("mListenerInfo");
+					f.setAccessible(true);
+					
+					Object obj = f.get(btn);
+					Class clazzView$ListenerInfo = Class.forName("android.view.View$ListenerInfo");
+					
+					Field f2= clazzView$ListenerInfo.getDeclaredField("mOnClickListener");
+					f2.setAccessible(true);
+					
+					Object obj2 = f2.get(obj);
+					System.out.println(obj);
+					System.out.println(obj2);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+		setContentView(btn);
+	}
+	
+	/**
+	 * android sdk中view的 ListenerInfo信息
+	 * @author kehongyun
+	 *
+	 */
+	static class ListenerInfo {
+        /**
+         * Listener used to dispatch focus change events.
+         * This field should be made private, so it is hidden from the SDK.
+         * {@hide}
+         */
+        protected OnFocusChangeListener mOnFocusChangeListener;
+
+        /**
+         * Listeners for layout change events.
+         */
+        private ArrayList<OnLayoutChangeListener> mOnLayoutChangeListeners;
+
+        /**
+         * Listeners for attach events.
+         */
+        private CopyOnWriteArrayList<OnAttachStateChangeListener> mOnAttachStateChangeListeners;
+
+        /**
+         * Listener used to dispatch click events.
+         * This field should be made private, so it is hidden from the SDK.
+         * {@hide}
+         */
+        public OnClickListener mOnClickListener;
+
+        /**
+         * Listener used to dispatch long click events.
+         * This field should be made private, so it is hidden from the SDK.
+         * {@hide}
+         */
+        protected OnLongClickListener mOnLongClickListener;
+
+        /**
+         * Listener used to build the context menu.
+         * This field should be made private, so it is hidden from the SDK.
+         * {@hide}
+         */
+        protected OnCreateContextMenuListener mOnCreateContextMenuListener;
+
+        private OnKeyListener mOnKeyListener;
+
+        private OnTouchListener mOnTouchListener;
+
+        private OnHoverListener mOnHoverListener;
+
+        private OnGenericMotionListener mOnGenericMotionListener;
+
+        private OnDragListener mOnDragListener;
+
+        private OnSystemUiVisibilityChangeListener mOnSystemUiVisibilityChangeListener;
+    }
  
 }
